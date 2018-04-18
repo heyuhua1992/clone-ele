2168f53af18e347bb78e8b53d6cf8124df9f2ae5
HeYuhua <heyuhua1992@163.com>
2018-04-18 17:28:56 +0800

    md 更改了代理路由，但没有效果，还差一个代理的服务器

--- config/index.js
+++ config/index.js
@@ -6,11 +6,27 @@ const path = require('path')
 
 module.exports = {
   dev: {
-
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+      // 还是不可用状态。。
+      '/proxy/data': {
+        // 请求数据的地址
+        targer: 'http://ustbhuangyi.com/sell/api/goods',
+        // 在本地会创建一个虚拟服务端，然后发送请求的数据，
+        // 并同时接收请求的数据，这样服务端和服务端进行数据
+        // 的交互就不会有跨域问题
+        changeOrigin: true,
+        // 路径重写
+        pathRewrite: {
+          // 替换target中的请求地址
+          // 生成的代理路径是：http://ustbhuangyi.com/sell/api/goods/data
+          // 把地址中的/data去掉
+          '^/proxy/data': ''
+        }
+      }
+    },
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
