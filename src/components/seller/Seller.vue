19abb464ac0a18f0845a5809726810294e04967b
HeYuhua <heyuhua1992@163.com>
2018-04-18 11:32:59 +0800

    大体页面差不多搞定，还差一点写完

--- src/components/seller/Seller.vue
+++ src/components/seller/Seller.vue
@@ -1,13 +1,214 @@
 <template>
-<div></div>
+<div class="seller" ref="seller">
+  <div class="seller-content">
+    <div class="overview">
+      <h1 class="title">
+        {{ seller.name }}
+      </h1>
+      <div class="desc border-1px">
+        <Star class="star" :size="36" :score="seller.score"/>
+        <span class="text">({{ seller.ratingCount }})</span>
+        <span class="text">月售{{ seller.sellerCount }}单</span>
+      </div>
+      <div>
+        <ul class="remark">
+          <li class="block">
+            <h2>起送价</h2>
+            <div class="content">
+              <span class="stress">{{ seller.minPrice }}</span>元
+            </div>
+          </li>
+          <li class="block">
+            <h2>商家配送</h2>
+            <div class="content">
+              <span class="stress">{{ seller.deliveryPrice }}</span>元
+            </div>
+          </li>
+          <li class="block">
+            <h2>平均配送时间</h2>
+            <div class="content">
+              <span class="stress">{{ seller.deliveryTime }}</span>分钟
+            </div>
+          </li>
+        </ul>
+        <div class="favorite" @click="toggleFavorite">
+          <span class="icon-favorite" :class="{'acitve': favorite}"></span>
+          <span class="text">{{ favoriteText }}</span>
+        </div>
+      </div>
+    </div>
+    <Split />
+    <div class="pics">
+      <h1 class="title">
+        商家实景
+      </h1>
+      <div class="pic-wrapper" ref="picWrapper">
+        <ul class="pic-list" ref="picList">
+          <li class="pic-item"
+              v-for="(pic, index) in seller.pics"
+              :key="index">
+            <img :src="pic" width="120" height="90"/>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <Split />
+    <div class="info">
+      <h1 class="title border-1px">商家信息</h1>
+      <ul>
+        <li class="info-item"
+            v-for="(item, index) in seller.infos"
+            :key="index">
+          {{ item }}
+        </li>
+      </ul>
+    </div>
+  </div>
+</div>
 </template>
 
 <script>
+import Star from 'components/star/Star'
+import Split from 'components/split/Split'
+import {saveToLocal, loadFromLocal} from 'common/js/store'
 export default {
-  name: 'seller'
+  name: 'seller',
+  props: {
+    seller: {
+      type: Object
+    }
+  },
+  data () {
+    return {
+      favorite: () => {
+        this.seller.id = 'id'
+        return loadFromLocal(this.seller.id, 'favorite', false)
+      }
+    }
+  },
+  methods: {
+    toggleFavorite () {
+      this.favorite = !this.favorite
+      this.seller.id = 'id'
+      saveToLocal(this.seller.id, 'favorite', this.favorite)
+    }
+  },
+  computed: {
+    favoriteText () {
+      return this.favorite ? '已收藏' : '收藏'
+    }
+  },
+  components: {
+    Star,
+    Split
+  }
 }
 </script>
 
-<style scoped>
-
+<style lang="stylus" rel="stylesheet/stylus" scoped>
+@import "../../common/stylus/mixin.styl"
+.seller
+  position absolute
+  top 180px
+  bottom 0
+  left 0
+  right 0
+  overflow hidden
+  .seller-content
+    .overview
+      position relative
+      padding 18px
+      .title
+        margin-bottom 8px
+        line-height 14px
+        color rgb(7, 17, 27)
+        font-size 14px
+      .desc
+        padding-bottom 18px
+        border-1px(rgba(7, 17, 27, .1))
+        font-size 0
+        .star
+          display inline-block
+          margin-right 8px
+          vertical-align top
+        .text
+          display inline-block
+          margin-right 12px
+          line-height 18px
+          font-size 12px
+          color rgb(77, 85, 93)
+          vertical-align top
+      .remark
+        display flex
+        padding-top 18px
+        .block
+          flex 1
+          text-align center
+          border-right 1px solid rgba(7, 17, 27, .1)
+          &:last-child
+            border-right none
+          h2
+            margin-bottom 4px
+            line-height 10px
+            font-size 10px
+            color rgb(147, 153, 159)
+          .content
+            line-height 24px
+            font-size 10px
+            color rgb(7, 17, 27)
+            .stress
+              font-size 24px
+      .favorite
+        position absolute
+        right 11px
+        top 18px
+        width 50px
+        text-align center
+        .icon-favorite
+          display block
+          margin-bottom 4px
+          font-size 24px
+          line-height 24px
+          color #d4d6d9
+          &.acitve
+            color rgb(240, 20, 20)
+        .text
+          line-height 10px
+          font-size 10px
+          color rgb(77, 85, 93)
+    .pics
+      padding 18px
+      .title
+        margin-bottom 12px
+        line-height 14px
+        font-size 14px
+        color rgb(7, 17, 27)
+      .pic-wrapper
+        width 100%
+        overflow hidden
+        white-space nowrap
+        .pic-list
+          font-size 0
+          .pic-item
+            display inline-block
+            margin-right 6px
+            width 120px
+            height 90px
+            &:last-child
+              margin-right 0
+    .info
+      padding 18px
+      color rgb(7, 17, 27)
+      .title
+        padding-bottom 12px
+        border-1px(rgba(7, 17, 27, .1))
+        line-height 14px
+        font-size 14px
+        .info-item
+          padding 16px 12px
+          line-height 16px
+          border-1px(rgba(7, 17, 27, .1))
+          font-size 12px
+          &:last-child
+            border-none
 </style>
